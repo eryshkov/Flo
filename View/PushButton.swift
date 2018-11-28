a3006c22f0a7782bcd3b355b201eb2289afbad1a
Evgeniy Ryshkov <eugem@macbook-pro-evgeniy.local>
2018-11-28 12:16:42 +0300

    button prepared to magic

---
+++ View/PushButton.swift
@@ -0,0 +1,21 @@
+//
+//  PushButton.swift
+//  Flo
+//
+//  Created by Evgeniy Ryshkov on 28/11/2018.
+//  Copyright © 2018 Evgeniy Ryshkov. All rights reserved.
+//
+
+import UIKit
+
+class PushButton: UIButton {
+
+    
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    
+
+}

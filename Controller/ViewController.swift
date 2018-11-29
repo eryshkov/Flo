380fa6a4d04938aaba25ffb1396d9820b59543eb
Evgeniy Ryshkov <eugem@macbook-pro-evgeniy.local>
2018-11-29 21:11:10 +0300

    animation added

--- Controller/ViewController.swift
+++ Controller/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var isGraphViewShowing = false
+    
     //Counter outlets
     @IBOutlet weak var counterView: CounterView!
     @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var graphView: GraphView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +34,30 @@ class ViewController: UIViewController {
             }
         }
         counterLabel.text = String(counterView.counter)
+        
+        if isGraphViewShowing {
+            counterViewTap(nil)
+        }
+    }
+    
+    
+    @IBAction func counterViewTap(_ gesture: UITapGestureRecognizer?) {
+        if (isGraphViewShowing) {
+            //hide Graph
+            UIView.transition(from: graphView,
+                              to: counterView,
+                              duration: 1.0,
+                              options: [.transitionFlipFromLeft, .showHideTransitionViews],
+                              completion:nil)
+        } else {
+            //show Graph
+            UIView.transition(from: counterView,
+                              to: graphView,
+                              duration: 1.0,
+                              options: [.transitionFlipFromRight, .showHideTransitionViews],
+                              completion: nil)
+        }
+        isGraphViewShowing = !isGraphViewShowing
     }
 
 }
---
+++ View/GraphView.swift
@@ -0,0 +1,21 @@
+//
+//  GraphView.swift
+//  Flo
+//
+//  Created by Evgeniy Ryshkov on 29/11/2018.
+//  Copyright © 2018 Evgeniy Ryshkov. All rights reserved.
+//
+
+import UIKit
+
+class GraphView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

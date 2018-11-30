56df649219131f706e2845144bd8f384223c1991
Evgeniy Ryshkov <eugem@macbook-pro-evgeniy.local>
2018-11-30 13:18:59 +0300

    background done

--- View/BackgroundView.swift
+++ View/BackgroundView.swift
@@ -25,5 +25,45 @@ class BackgroundView: UIView {
         
         //4
         context.fill(rect)
+        
+        let drawSize = CGSize(width: patternSize, height: patternSize)
+        
+        UIGraphicsBeginImageContextWithOptions(drawSize, true, 0.0)
+        let drawingContext = UIGraphicsGetCurrentContext()!
+        
+        //set the fill color for the new context
+        darkColor.setFill()
+        drawingContext.fill(CGRect(x: 0, y: 0, width: drawSize.width, height: drawSize.height))
+        
+        let trianglePath = UIBezierPath()
+        //1
+        trianglePath.move(to: CGPoint(x: drawSize.width/2, y: 0))
+        //2
+        trianglePath.addLine(to: CGPoint(x: 0, y: drawSize.height/2))
+        //3
+        trianglePath.addLine(to: CGPoint(x: drawSize.width, y: drawSize.height/2))
+        
+        //4
+        trianglePath.move(to: CGPoint(x: 0,y: drawSize.height/2))
+        //5
+        trianglePath.addLine(to: CGPoint(x: drawSize.width/2, y: drawSize.height))
+        //6
+        trianglePath.addLine(to: CGPoint(x: 0, y: drawSize.height))
+        
+        //7
+        trianglePath.move(to: CGPoint(x: drawSize.width, y: drawSize.height/2))
+        //8
+        trianglePath.addLine(to: CGPoint(x: drawSize.width/2, y: drawSize.height))
+        //9
+        trianglePath.addLine(to: CGPoint(x: drawSize.width, y: drawSize.height))
+        
+        lightColor.setFill()
+        trianglePath.fill()
+        
+        let image = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        UIColor(patternImage: image).setFill()
+        context.fill(rect)
     }
 }
